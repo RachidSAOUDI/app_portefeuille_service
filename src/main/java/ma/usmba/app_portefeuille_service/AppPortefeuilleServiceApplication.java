0e62157fbd66999fce78f9f059cb8211bde6aae4
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-02-23 15:10:29 +0100

    initial commit

---
+++ src/main/java/ma/usmba/app_portefeuille_service/AppPortefeuilleServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.usmba.app_portefeuille_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPortefeuilleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPortefeuilleServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/usmba/app_portefeuille_service/AppPortefeuilleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usmba.app_portefeuille_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPortefeuilleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
